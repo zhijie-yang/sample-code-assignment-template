d0f90733768e30e2a1e48800da7a798a1bfa1f2c
Zhijie Yang <zhijie.yang1@outlook.com>
2025-08-31 02:32:04 +0200

    feat: initial commit

---
+++ src/math/calculator.py
@@ -0,0 +1,63 @@
+class Calculator:
+    @staticmethod
+    def add(a: int | float, b: int | float) -> int | float:
+        """
+        Adds two numbers.
+
+        Args:
+            a (int | float): Operand a.
+            b (int | float): Operand b.
+
+        Returns:
+            int | float: The sum of a and b.
+        """
+
+        raise NotImplementedError()
+
+    @staticmethod
+    def subtract(a: int | float, b: int | float) -> int | float:
+        """
+        Subtracts b from a.
+
+        Args:
+            a (int | float): Operand a.
+            b (int | float): Operand b.
+
+        Returns:
+            int | float: The result of a - b.
+        """
+
+        raise NotImplementedError()
+
+    @staticmethod
+    def multiply(a: int | float, b: int | float) -> int | float:
+        """
+        Multiplies two numbers.
+
+        Args:
+            a (int | float): Operand a.
+            b (int | float): Operand b.
+
+        Returns:
+            int | float: The product of a and b.
+        """
+
+        raise NotImplementedError()
+
+    @staticmethod
+    def divide(a: int | float, b: int | float) -> float:
+        """
+        Divides a by b.
+
+        Args:
+            a (int | float): Operand a.
+            b (int | float): Operand b.
+
+        Raises:
+            ValueError: Upon division by zero.
+
+        Returns:
+            float: The result of a / b.
+        """
+
+        raise NotImplementedError()
---
+++ tests/unit/calculator/test_calculator.py
@@ -0,0 +1,67 @@
+import pytest
+
+from src.math.calculator import Calculator as C
+
+class TestCalculator:
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (1, 2, 3),
+            (-1, -1, -2),
+            (0, 0, 0),
+            (1.5, 2.5, 4.0),
+            (-1.5, 1.5, 0.0),
+        ],
+    )
+    def test_add(self, a, b, expected):
+        assert C.add(a, b) == expected
+
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (2, 1, 1),
+            (-1, -1, 0),
+            (0, 0, 0),
+            (2.5, 1.5, 1.0),
+            (-1.5, 1.5, -3.0),
+        ],
+    )
+    def test_subtract(self, a, b, expected):
+        assert C.subtract(a, b) == expected
+
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (2, 3, 6),
+            (-1, -1, 1),
+            (0, 10, 0),
+            (2.5, 2.0, 5.0),
+            (-1.5, 2.0, -3.0),
+        ],
+    )
+    def test_multiply(self, a, b, expected):
+        assert C.multiply(a, b) == expected
+
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (6, 3, 2.0),
+            (-4, -2, 2.0),
+            (5.0, 2.0, 2.5),
+            (-4.5, -1.5, 3.0),
+        ],
+    )
+    def test_divide(self, a, b, expected):
+        assert C.divide(a, b) == expected
+
+    @pytest.mark.parametrize(
+        "a, b",
+        [
+            (1, 0),
+            (-1, 0),
+            (0, 0),
+        ],
+    )
+    def test_divide_by_zero(self, a, b):
+        with pytest.raises(ValueError):
+            C.divide(a, b)
